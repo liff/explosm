367720223f685b83a17ac1caaeb43d5eb537bc3d
Olli Helenius <liff@iki.fi>
2013-01-25 18:38:13 +0200

    Hello world (in 2D)

--- src/main/java/Blowjob.java
+++ src/main/java/Blowjob.java
@@ -1,5 +1,29 @@
-public class Blowjob {
-    public static void main(final String[] args) {
-        System.out.println("hello");
+import org.newdawn.slick.*;
+
+public class Blowjob extends BasicGame {
+    public Blowjob() {
+        super("Blowjob");
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        g.drawString("Hello World", 100, 100);
+    }
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new Blowjob());
+
+        app.setDisplayMode(800, 600, false);
+        app.start();
     }
 }
