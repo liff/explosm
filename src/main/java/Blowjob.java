b4358ac016d28f2cb1ce936f3649163f61733fcb
Jani Lämsä <janilamsa89@gmail.com>
2013-01-26 12:10:38 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/Blowjob.java

--- src/main/java/Blowjob.java
+++ src/main/java/Blowjob.java
@@ -57,40 +57,40 @@ public class Blowjob extends BasicGame {
             }
             public void onBeat(final double phase) {
                 System.out.println("phase " + phase);
-                beat1.play();
+                beat1.play(1.0f, 1.0f) ;
             }
         });
         player.addBeatListener(new Heart.BeatListener() {
             public double phase() {
-                return 0.6;
+                return 0.4;
             }
             public void onBeat(final double phase) {
                 System.out.println("phase " + phase);
                 beat2.play();
             }
         });
-        heart.speed = 10; // XXX
+        heart.speed = 6; // XXX
         System.out.println("heart duration is " + player.getBeatDuration());
         input = gc.getInput();
 
-        beat2 = new Sound("src/main/resources/beat2.wav");
-        double beatDuration = player.getBeatDuration();
-
-        timeBetweenBeats = 1000;
-
-        //Pitäis saada tapahtumaan jokaisen sykkeen alussa
-        beatTimer1 = beatDuration/10;
-
-        beatTimer2 = beatDuration/3;
-        beat1Length = 182;
-        beat2Length = 285;
-        test = beat1Length / beatDuration;
-        test2 = beat2Length / beatDuration;
-
-        System.out.println("test is " +test);
-        //beat2.loop((float)test, 10.0f);
-
-        beatsPlayed = false;
+        //beat2 = new Sound("src/main/resources/beat2.wav");
+        //double beatDuration = player.getBeatDuration();
+        //
+        //timeBetweenBeats = 1000;
+        //
+        ////Pitäis saada tapahtumaan jokaisen sykkeen alussa
+        //beatTimer1 = beatDuration/10;
+        //
+        //beatTimer2 = beatDuration/3;
+        //beat1Length = 182;
+        //beat2Length = 285;
+        //test = beat1Length / beatDuration;
+        //test2 = beat2Length / beatDuration;
+        //
+        //System.out.println("test is " +test);
+        ////beat2.loop((float)test, 10.0f);
+        //
+        //beatsPlayed = false;
     }
 
     @Override
@@ -102,29 +102,6 @@ public class Blowjob extends BasicGame {
         }
         applyCuts();
 
-        timeFlown += delta;
-
-        beatTimer1 -= delta;
-
-
-
-       if (beatTimer1 <= 0 && beatsPlayed == false) {
-           //beat1.play((float)test, 10.0f);
-           beatTimer1 += player.getBeatDuration(); //tapa minut
-       }
-
-       beatTimer2 -= delta;
-
-       if (beatTimer2 <= 0 && beatsPlayed == false) {
-           //beat2.play((float)test2, 10.0f);
-           beatTimer2 += player.getBeatDuration(); //lopeta tuskani
-       }
-
-       // if(timeFlown >= (player.getBeatDuration()/10 + beat1Length + player.getBeatDuration()/3 + beat2Length) ) {
-       //     beatTimer1 = player.getBeatDuration()/10;
-       //     beatTimer2 = player.getBeatDuration()/3;
-       //
-       // }
 
 
     }
